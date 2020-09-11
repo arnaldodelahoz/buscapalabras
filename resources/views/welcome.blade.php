451f6c774d1839550b2580b95fc12e65a99b5376
Arnaldo De la hoz <arnaldodelahoz@gmail.com>
2020-09-11 12:11:41 -0400

    update css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,19 +8,17 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="{{ secure_asset('dist/favicon.ico') }}">
-    <link rel="stylesheet" href="{{ secure_asset('dist/css/app.21f2b4ee.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('dist/css/chunk-vendors.84bb20f7.css') }}">
-    <link rel=preload as=script href="{{ secure_asset('dist/js/app.d8c25f5f.js') }}">
+    <link rel=preload as=script href="{{ secure_asset('dist/js/app.1f0d73a2.js') }}">
     <link rel=preload as=script href="{{ secure_asset('dist/js/chunk-vendors.92f920e5.js ') }}">
     <link rel="stylesheet" href="{{ secure_asset('dist/css/chunk-vendors.84bb20f7.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('dist/css/app.5cef3ca8.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('dist/css/app.b213f6b6.css') }}">
 
 </head>
 
 <body>
     <div id="app"></div>
     <script src="{{ secure_asset('dist/js/chunk-vendors.92f920e5.js') }}"></script>
-    <script src="{{ secure_asset('dist/js/app.d8c25f5f.js') }}"></script>
+    <script src="{{ secure_asset('dist/js/app.1f0d73a2.js') }}"></script>
 </body>
 
 </html>
