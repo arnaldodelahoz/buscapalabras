a1a375aa40b4eee43c26d381a9f7f283aa558b0a
Arnaldo De la hoz <arnaldodelahoz@gmail.com>
2020-09-11 04:20:07 -0400

    init commit

---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Word;
+use Illuminate\Http\Request;
+use Arr;
+class WordController extends Controller
+{
+    public function search(Request $request, $letters, $num,$startWith){
+        $searchParams = $request->all();
+        $limit = Arr::get($searchParams, 'limit', 500);
+        $num=is_int(intval($num))?$num:4;
+        $exp = "^[" . strtolower(normalizer_normalize($letters)) . "]*$";
+        $query = Word::where('word', 'regexp', $exp)->whereRaw('LENGTH(word) ='.$num);
+        if($startWith && $startWith !== '--'){
+            $query->where('word','like', $startWith.'%');
+        }
+            return $query->paginate($limit);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <title>EF TEST</title>
+    <base href="/">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{ asset('dist/favicon.ico') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/app.21f2b4ee.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/chunk-vendors.84bb20f7.css') }}">
+    <link rel=preload as=script href="{{ asset('dist/js/app.7d438ee8.js') }}">
+    <link rel=preload as=script href="{{ asset('dist/js/chunk-vendors.eb11dc7b.js ') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/chunk-vendors.84bb20f7.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/app.21f2b4ee.css') }}">
+
+</head>
+
+<body>
+    <div id="app"></div>
+    <script src="{{ asset('dist/js/chunk-vendors.eb11dc7b.js') }}"></script>
+    <script src="{{ asset('dist/js/app.7d438ee8.js') }}"></script>
+</body>
+
+</html>
